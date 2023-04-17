8c3edbbd2523fa4559ffe15101cad283276fbb90
Matsvei Liapich <hello@matsveil.com>
2023-04-17 16:01:52 -0400

    Change cursor to hollow circle on hover adn optimize for mobile

--- cursor.js
+++ cursor.js
@@ -2,19 +2,22 @@ const cursor = document.querySelector('.cursor');
 
 const showCursor = () => {
   cursor.style.display = 'block';
-}
+};
 
-const moveCursor = (e)=> {
+const moveCursor = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
-   
-  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`; 
-}
+
+  // Check if the mouse is hovering over a link element
+  if (e.target.tagName === 'A') {
+    cursor.classList.add('hollow');
+  } else {
+    cursor.classList.remove('hollow');
+  }
+
+  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
 
 window.addEventListener('mousemove', showCursor);
 window.addEventListener('mousemove', moveCursor);
-
-
-
-  
 
