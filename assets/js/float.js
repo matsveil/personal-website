4646a7c5ad8851bac3312c4d5a95686b47525a38
Matsvei Liapich <hello@matsveil.com>
2023-04-19 10:08:32 -0400

    Optimize gradient blob

--- assets/js/float.js
+++ assets/js/float.js
@@ -1,43 +1,31 @@
-$(document).ready(function () {
+$(function () {
+  var $blob = $(".blob");
+
+  function makeNewPosition() {
+    var h = window.innerHeight - 1000;
+    var w = window.innerWidth - 1000;
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    return { top: nh, left: nw };
+  }
+
+  function animateDiv() {
+    var newq = makeNewPosition();
+    var oldq = $blob.offset();
+    var speed = calcSpeed(oldq, newq);
+    $blob.animate(newq, speed, "linear", function () {
+      window.requestAnimationFrame(animateDiv);
+    });
+  }
+
+  function calcSpeed(prev, next) {
+    var x = Math.abs(prev.left - next.left);
+    var y = Math.abs(prev.top - next.top);
+    var greatest = x > y ? x : y;
+    var speedModifier = 0.03;
+    var speed = Math.floor(greatest / speedModifier);
+    return speed;
+  }
+
   animateDiv();
-
 });
-
-function makeNewPosition() {
-
-  // Get viewport dimensions (remove the dimension of the div)
-  var h = $(window).height() - 1000;
-  var w = $(window).width() - 1000;
-
-  var nh = Math.floor(Math.random() * h);
-  var nw = Math.floor(Math.random() * w);
-
-  return [nh, nw];
-
-}
-
-function animateDiv() {
-  var newq = makeNewPosition();
-  var oldq = $(".blob").offset();
-  var speed = calcSpeed([oldq.top, oldq.left], newq);
-
-  $(".blob").animate({ top: newq[0], left: newq[1] }, speed, function () {
-    animateDiv();
-  });
-
-};
-
-function calcSpeed(prev, next) {
-
-  var x = Math.abs(prev[1] - next[1]);
-  var y = Math.abs(prev[0] - next[0]);
-
-  var greatest = x > y ? x : y;
-
-  var speedModifier = 0.03;
-
-  var speed = Math.ceil(greatest / speedModifier);
-
-  return speed;
-
-}
