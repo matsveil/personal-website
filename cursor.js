1f4a2d78e9d3d46ed65c0ccededa1a6734400276
Matsvei Liapich <hello@matsveil.com>
2023-04-18 22:00:05 -0400

    Optimize custom cursor for Firefox and Safari

--- cursor.js
+++ cursor.js
@@ -1,4 +1,9 @@
 const cursor = document.querySelector('.cursor');
+const cursorPos = document.createElement('div');
+cursorPos.style.position = 'fixed';
+cursorPos.style.top = '-100px';
+cursorPos.style.left = '-100px';
+document.body.appendChild(cursorPos);
 
 const showCursor = () => {
   cursor.style.display = 'block';
@@ -7,17 +12,24 @@ const showCursor = () => {
 const moveCursor = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
+  const target = e.target;
 
   // Check if the mouse is hovering over a link element
-  if (e.target.tagName === 'A') {
+  if (target.tagName === 'A') {
     cursor.classList.add('hollow');
   } else {
     cursor.classList.remove('hollow');
   }
 
-  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  cursorPos.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+const updateCursor = () => {
+  const transformValue = cursorPos.style.transform;
+  cursor.style.transform = transformValue;
+  requestAnimationFrame(updateCursor);
 };
 
 window.addEventListener('mousemove', showCursor);
 window.addEventListener('mousemove', moveCursor);
-
+requestAnimationFrame(updateCursor);
